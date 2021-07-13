c14daafba8c256ce27509a422c0fb6d68e1993f2
Vitor Gomes <vgomes@Vitors-MBP.lan>
2021-07-13 13:56:32 +0100

    improve designs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,18 @@ import * as React from "react";
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
+  display: "flex",
+  justifyContent: "center",
+  padding: 32,
+};
+const contentStyles = {
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  color: "#232129",
+  maxWidth: 600,
 };
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
-  maxWidth: 500,
 };
 const headingAccentStyles = {
   color: "#45B69C",
@@ -30,25 +34,27 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Beyond Bits</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>
-          — you just spotted a wonderful opportunity!{" "}
-        </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Do you want this domain?{" "}
-        <a style={linkStyles} href="mailto:vitor.gomes@algoritmoprudente.com">
-          Please reach out!
-        </a>
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
+      <section style={contentStyles}>
+        <h1 style={headingStyles}>
+          Congratulations
+          <br />
+          <span style={headingAccentStyles}>
+            — you just spotted a wonderful opportunity!{" "}
+          </span>
+          <span role="img" aria-label="Party popper emojis">
+            🎉🎉🎉
+          </span>
+        </h1>
+        <p style={paragraphStyles}>
+          Do you want this domain?{" "}
+          <a style={linkStyles} href="mailto:vitor.gomes@algoritmoprudente.com">
+            Please reach out!
+          </a>
+          <span role="img" aria-label="Sunglasses smiley emoji">
+            😎
+          </span>
+        </p>
+      </section>
     </main>
   );
 };
